666829cb67cd650f7e15dd5b3271bc37bce9fb8a
Jakub Wojtyra <wojtyraj@gmail.com>
2018-05-25 21:23:07 +0200

    Delete unnecessary comment

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-//ENABLE CORS
-
 io.on('connection', function (socket) {
   console.log('new user connected');
 });
